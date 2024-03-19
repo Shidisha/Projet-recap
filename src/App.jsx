838068cea361d72efca0eb53c1ff1901515dcaac
Djo tshibangu <137219430+Shidisha@users.noreply.github.com>
2024-03-19 15:25:13 +0000

    installing and importing react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import './App.css'
+import Home from './Home'
+import Contact from './Contact'
 
 export default function App() {
   return (
-    <main>
-      React ⚛️ + Vite ⚡ + Replit
-    </main>
+    <Router>
+      <Switch>
+        <Route exact path='/' Component={Home}/>
+        <Route path='/Contact' Component={Contact}/>
+      </Switch>      
+    </Router>
   )
 }
+
+export default App;
